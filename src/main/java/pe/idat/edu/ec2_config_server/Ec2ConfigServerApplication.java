3e9b7cae0efcf2422ed1ebaee6b4f290e7084515
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-02-23 10:08:37 -0500

    configuracion pom

---
+++ src/main/java/pe/idat/edu/ec2_config_server/Ec2ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.ec2_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2ConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/edu/ec2_config_server/Ec2ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.ec2_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
